8d639fd58d2ffb04b46eb7293725542425b700f6
daebeobkim <109015772+daebeobkim@users.noreply.github.com>
2023-03-07 08:56:22 +0900

    Update rr.py

--- rr.py
+++ rr.py
@@ -1,10 +1,38 @@
-import pandas as pd
-df = pd.read_csv('C:/Users/rlaeo/OneDrive/바탕 화면/iris.csv', names = ["sepal_length", "sepal_width", "petal_length", "petal_width", "species"])
-print(df.head())
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 
-import seaborn as sns
+import pandas as pd
+import numpy
+import tensorflow as tf
 import matplotlib.pyplot as plt
 
+# seed 값 설정
+seed = 0
+numpy.random.seed(seed)
+tf.random.set_seed(3)
 
-f = sns.pairplot(df, hue="species");
-plt.show()
+# 데이터 입력
+df_pre = pd.read_csv('C:/Users/rlaeo/OneDrive/바탕 화면/wine.csv', header=None)
+df = df_pre.sample(frac=1)
+dataset = df.values
+X = dataset[:,0:12]
+Y = dataset[:,0:12]    #  무슨 오류일까?
+
+# 모델 설정
+model = Sequential()
+model.add(Dense(30, input_dim=12, activation='relu'))
+model.add(Dense(12, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+# 모델 컴파일 
+# binary_crossentropy = 오차함수
+# adam() = 최적화 함수
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# 모델 실행
+model.fit(X, Y, epochs=200, batch_size=200)
+
+# 결과 출력
+print("\n Accuracy: %.4f" % (model.evaluate(X, Y)[1]))
